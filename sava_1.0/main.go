eb69e7fa632888f08d441e7a0f77638746430663
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-12-09 11:36:36 +0100

    adding debug info

--- sava_1.0/main.go
+++ sava_1.0/main.go
@@ -6,11 +6,15 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"crypto/rand"
+	"fmt"
 )
 
 var (
-	port = flag.Int("port", 8080, "Sets the port to listen on")
+	version = "sava:1.0"
+	uuid = generate_uuid()
 	debug = flag.Bool("debug", false, "Sets the debug mode")
+	port = flag.Int("port", 8080, "Sets the port to listen on")
 )
 
 func main() {
@@ -37,11 +41,41 @@ func parameters() {
 
 func server() {
 
-	r := gin.Default()
-	r.Static("/public", "./public")
-	r.GET("/", func(c *gin.Context) {
-		c.Redirect(http.StatusMovedPermanently, "/public")
+	router := gin.Default()
+
+	router.LoadHTMLGlob("index.tmpl")
+	router.Static("/public/js", "./public/js")
+	router.Static("/public/css", "./public/css")
+
+	router.GET("/", func(c *gin.Context) {
+		var display = "none"
+		if *debug {
+			display = "block"
+		}
+		obj := gin.H{"display": display, "version": version, "uuid": uuid}
+		c.HTML(200, "index.tmpl", obj)
 	})
 
-	r.Run(":" + strconv.Itoa(*port))
+	router.GET("/public", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/")
+	})
+
+	router.GET("public/favicon.png", func(c *gin.Context) {
+		c.File("./public/favicon.png")
+	})
+
+	router.Run(":" + strconv.Itoa(*port))
+}
+
+func generate_uuid() (uuid string) {
+
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	uuid = fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+	return
 }
--- sava_1.1/main.go
+++ sava_1.1/main.go
@@ -6,11 +6,23 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"crypto/rand"
+	"fmt"
+)
+
+var (
+	version = "sava:1.1"
+	uuid = generate_uuid()
+	debug = flag.Bool("debug", false, "Sets the debug mode")
+	port = flag.Int("port", 8080, "Sets the port to listen on")
 )
 
 func main() {
+	parameters()
+	server()
+}
 
-	port := flag.Int("port", 8080, "Sets the port to listen on")
+func parameters() {
 
 	flag.Parse()
 
@@ -20,11 +32,50 @@ func main() {
 		*port, _ = strconv.Atoi(envPort)
 	}
 
-	r := gin.Default()
-	r.Static("/public", "./public")
-	r.GET("/", func(c *gin.Context) {
-		c.Redirect(http.StatusMovedPermanently, "/public")
+	envDebug := os.Getenv("SAVA_DEBUG")
+
+	if envDebug != "" {
+		*debug, _ = strconv.ParseBool(envDebug)
+	}
+}
+
+func server() {
+
+	router := gin.Default()
+
+	router.LoadHTMLGlob("index.tmpl")
+	router.Static("/public/js", "./public/js")
+	router.Static("/public/css", "./public/css")
+
+	router.GET("/", func(c *gin.Context) {
+		var display = "none"
+		if *debug {
+			display = "block"
+		}
+		obj := gin.H{"display": display, "version": version, "uuid": uuid}
+		c.HTML(200, "index.tmpl", obj)
 	})
 
-	r.Run(":" + strconv.Itoa(*port))
+	router.GET("/public", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/")
+	})
+
+	router.GET("public/favicon.png", func(c *gin.Context) {
+		c.File("./public/favicon.png")
+	})
+
+	router.Run(":" + strconv.Itoa(*port))
+}
+
+func generate_uuid() (uuid string) {
+
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	uuid = fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+	return
 }
